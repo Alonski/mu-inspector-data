8a94210ce8b6a5fcbb50879d71f11e916e86db4b
Alon Bukai <alon@camilyo.com>
2018-09-21 13:39:48 +0300

    Get minimal MU app working with Ember Data

--- src/resolver.js
+++ src/resolver.js
@@ -2,11 +2,31 @@ import Resolver from 'ember-resolver/resolvers/fallback';
 import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 
-let moduleConfig = buildResolverConfig(config.modulePrefix);
+import { merge } from '@ember/polyfills';
+
+const moduleConfig = buildResolverConfig(config.modulePrefix);
 /*
  * If your application has custom types and collections, modify moduleConfig here
  * to add support for them.
  */
+
+merge(moduleConfig.types, {
+  config: { definitiveCollection: 'main' },
+  util: { definitiveCollection: 'utils' },
+  // ember-intl
+  'ember-intl@adapter': { definitiveCollection: 'main' },
+  'ember-intl@translation': { definitiveCollection: 'main' },
+  translation: { definitiveCollection: 'main' },
+  formats: { definitiveCollection: 'main' },
+  cldr: { definitiveCollection: 'main' },
+  'util:intl': { definitiveCollection: 'utils' },
+  // ember-gestures
+  'ember-gesture': { definitiveCollection: 'main' },
+  mixin: { definitiveCollection: 'main' }
+});
+
+moduleConfig.collections.main.types.push('config');
+moduleConfig.collections.main.types.push('translation');
 
 export default Resolver.extend({
   config: moduleConfig
